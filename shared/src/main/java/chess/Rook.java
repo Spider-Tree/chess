81828528009a2bc007e4615a21cd188968c80689
Dimitri <dimitriw@byu.edu>
2026-09-15 15:28:28 -0600

    Added Logic to Rook

--- shared/src/main/java/chess/Rook.java
+++ shared/src/main/java/chess/Rook.java
@@ -1,10 +1,48 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Rook {
+    ArrayList<ChessMove> allPos;
     public Rook(){}
     public Collection<ChessMove> pieceMoveRook(ChessBoard board, ChessPosition myPosition){
-        throw new RuntimeException("Not yet implemented.");
+        checkCol(board,myPosition,1);
+        checkCol(board,myPosition,-1);
+        checkRow(board,myPosition,1);
+        checkRow(board,myPosition,-1);
+        return allPos;
+    }
+    public void checkCol(ChessBoard b, ChessPosition start, int direction){
+        ChessGame.TeamColor color=b.getPiece(start).getTeamColor();
+        int col=start.getRow();
+        for (int r= start.getRow(); r<9 &&0<r;r+=direction){
+            if (null==b.getPiece(new ChessPosition(r,col))){
+                allPos.add(new ChessMove(start,new ChessPosition(r,col),null));
+            }
+            else if(color!=b.getPiece(new ChessPosition(r,col)).getTeamColor()){
+                allPos.add(new ChessMove(start,new ChessPosition(r,col),null));
+                r=12;
+            }
+            else{
+                r=12;
+            }
+        }
+    }
+    public void checkRow(ChessBoard b, ChessPosition start, int direction){
+        ChessGame.TeamColor color=b.getPiece(start).getTeamColor();
+        int row=start.getColumn();
+        for (int c= start.getColumn(); c<9 &&0<c;c+=direction){
+            if (null==b.getPiece(new ChessPosition(c,row))){
+                allPos.add(new ChessMove(start,new ChessPosition(c,row),null));
+            }
+            else if(color!=b.getPiece(new ChessPosition(c,row)).getTeamColor()){
+                allPos.add(new ChessMove(start,new ChessPosition(c,row),null));
+                c=12;
+            }
+            else{
+                c=12;
+            }
+        }
     }
 }
