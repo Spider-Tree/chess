4487b12ec74c5282a53136694a4b2c734fe21539
Dimitri <dimitriw@byu.edu>
2026-09-15 15:01:22 -0600

    Added Knight Logic

--- shared/src/main/java/chess/Knight.java
+++ shared/src/main/java/chess/Knight.java
@@ -22,16 +22,19 @@ public class Knight {
         ChessGame.TeamColor color=b.getPiece(start).getTeamColor();
         ChessPosition pos1= new ChessPosition((2*directionVert)+row,(directionHori)+row);
         ChessPosition pos2= new ChessPosition(directionVert+row,(2*directionHori)+col);
-
+        if (pos1.getRow()<9&&pos1.getColumn()<9&&0<pos1.getRow()&&0<pos1.getColumn()){
         if (null==b.getPiece(pos1)){
             allpos.add(new ChessMove(start,pos1,null));}
         else if(color!=b.getPiece(pos1).getTeamColor()){
             allpos.add(new ChessMove(start,pos1,null));
         }
-        if (null==b.getPiece(pos2)){
-            allpos.add(new ChessMove(start,pos1,null));}
-        else if(color!=b.getPiece(pos1).getTeamColor()){
-            allpos.add(new ChessMove(start,pos2,null));
+        }
+        if (pos2.getRow()<9&&pos2.getColumn()<9&&0<pos2.getRow()&&0<pos2.getColumn()) {
+            if (null == b.getPiece(pos2)) {
+                allpos.add(new ChessMove(start, pos1, null));
+            } else if (color != b.getPiece(pos1).getTeamColor()) {
+                allpos.add(new ChessMove(start, pos2, null));
+            }
         }
     }
 }
