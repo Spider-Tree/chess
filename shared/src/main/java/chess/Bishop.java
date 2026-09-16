225e2e4008abe9cda24e91ff2384600b086e1554
Dimitri <dimitriw@byu.edu>
2026-09-16 11:20:22 -0600

    Bishop passes Move Test

--- shared/src/main/java/chess/Bishop.java
+++ shared/src/main/java/chess/Bishop.java
@@ -19,18 +19,18 @@ public class Bishop {
     }
     public void checkDiagonal(ChessBoard b, ChessPosition start, int directionR, int directionC){
         ChessGame.TeamColor color = b.getPiece(start).getTeamColor();
-        for (int r = start.getRow(), c = start.getColumn(); c < 9 && r < 9 && 0<r && 0<c; c+=directionC, r+=directionR) {
+        for (int r = start.getRow()+directionR, c = start.getColumn()+directionC; c < 9 && r < 9 && 0<r && 0<c; c=directionC+c, r=directionR+r) {
+            //System.out.println(r+", "+ c);
 
-            if (null != b.getPiece(new ChessPosition(r, c))) {
-                ChessPiece other = b.getPiece(new ChessPosition(r, c));
-                if (other.getTeamColor() == color) {
-                    c = 12;
-                } else {
-                    allPos.add(new ChessMove(start, new ChessPosition(r, c), null));
-                    c = 12;
-                }
-            } else {
-                allPos.add(new ChessMove(start, new ChessPosition(r, c), null));
+            if (null == b.getPiece(new ChessPosition(r, c))) {
+                allPos.add(new ChessMove(start,new ChessPosition(r,c),null));
+            }
+            else if(color!=b.getPiece(new ChessPosition(r,c)).getTeamColor()){
+                allPos.add(new ChessMove(start, new ChessPosition(r,c),null));
+                c=14;
+            }
+            else{
+                c=14;
             }
 
 
