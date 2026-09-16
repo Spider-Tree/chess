d7818b22440d849c2f6bc16767aab06daade749e
Dimitri <dimitriw@byu.edu>
2026-09-16 08:34:27 -0600

    Add Queen logic that reuses bishop and rook logic.

--- shared/src/main/java/chess/Queen.java
+++ shared/src/main/java/chess/Queen.java
@@ -1,10 +1,16 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Queen {
+    ArrayList<ChessMove> allpos;
     public Queen(){}
     public Collection<ChessMove> pieceMoveQueen(ChessBoard board, ChessPosition myPosition){
-        throw new RuntimeException("Not yet implemented.");
+        Collection<ChessMove> straightLines = new Rook().pieceMoveRook(board,myPosition);
+        Collection<ChessMove> diagonals= new Bishop().pieceMoveBishop(board,myPosition);
+        allpos.addAll(straightLines);
+        allpos.addAll(diagonals);
+        return allpos;
     }
 }
